d2b895661dcb6445411d8676c2bd444a24b29c3a
amirCodes <amirdanish126@gmail.com>
2020-07-14 13:08:12 +0800

    first commit

---
+++ components/Joke.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "joke"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/aboutUs.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="container">
+    <div>
+      <h1 class="title"> About DadJoks</h1>
+      <div class="">
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+    head(){
+      return {
+          title: 'about page',
+          meta: [
+              {
+                  hid: "description",
+                  name: "description",
+                  content: "Best place for corny Dadjoks"
+              }
+          ]
+      }
+    }
+}
+</script>
+
+<style>
+
+</style>
