2fabe0e82395650039e669a362f91c18c6a1d527
amirCodes <amirdanish126@gmail.com>
2021-02-27 16:52:33 +0800

    added the searchengin

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,5 +1,18 @@
 <template>
-    $END$
+    <header class="header">
+      <h1 class="title"> Dad Jokes</h1>
+      <ul>
+        <li>
+          <nuxt-link to="/">Home</nuxt-link>
+        </li>
+        <li>
+          <nuxt-link to="/jokes">Jokes</nuxt-link>
+        </li>
+        <li>
+          <nuxt-link to="/aboutUs">About</nuxt-link>
+        </li>
+      </ul>
+    </header>
 </template>
 
 <script>
@@ -9,5 +22,29 @@
 </script>
 
 <style scoped>
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 1rem;
+  padding-bottom: 1rem;
+  border-bottom: 1px dotted #ccc;
+  border: #47494E
+}
 
+  .header .title {
+    font-size: 3rem;
+  }
+
+  .header ul {
+    display: flex;
+  }
+
+  .header a{
+    display: inline-block;
+    background: #47494E;
+    color: #F5F5F5;
+    padding: 0.3rem 1rem;
+    margin-right: 0.5rem;
+  }
 </style>
--- components/SearchJokes.vue
+++ components/SearchJokes.vue
@@ -1,13 +1,13 @@
 <template>
   <form @submit.prevent="onSubmit">
     <input type="text" v-model="text" placeholder="Search Jokes..." />
-    <input type="submit" value="Search Jokes" />
+    <input type="submit" value="Search Jokes" class="btn-primary" />
   </form>
 </template>
 
 <script>
     export default {
-        name: "SearchJoke",
+        name: "SearchJokes",
         data() {
             return {
                 text: ""
